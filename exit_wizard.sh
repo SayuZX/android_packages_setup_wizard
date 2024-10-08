5d72b2ec7d2aade9937355c7eec34df411dcd2b7
SayuZX <todraihan95@gmail.com>
2024-10-08 15:00:29 +0700

    Initial bringup for Android 13 Setup Wizard
    - Integrate setup wizard for Android 13
    - Implement initial flow and UI
    - Add support for essential setup components

---
+++ exit_wizard.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+adb root
+wait ${!}
+adb shell pm enable org.scandiumos.setupwizard/org.scandiumos.setupwizard.SetupWizardExitActivity || true
+wait ${!}
+adb shell pm enable com.google.android.setupwizard/com.google.android.setupwizard.SetupWizardExitActivity || true
+wait ${!}
+sleep 1
+adb shell am start org.scandiumos.setupwizard/org.scandiumos.setupwizard.SetupWizardExitActivity || true
+wait ${!}
+sleep 1
+adb shell am start com.google.android.setupwizard/com.google.android.setupwizard.SetupWizardExitActivity
